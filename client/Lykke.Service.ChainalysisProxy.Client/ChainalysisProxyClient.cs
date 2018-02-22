c4b82628b53f8123580ed87a530f60dd08e0d84e
Anton Sivov <anton.sivov@inbox.ru>
2018-02-22 10:18:07 +0300

    Added comments

--- client/Lykke.Service.ChainalysisProxy.Client/ChainalysisProxyClient.cs
+++ client/Lykke.Service.ChainalysisProxy.Client/ChainalysisProxyClient.cs
@@ -7,17 +7,28 @@ using Lykke.Service.ChainalysisProxy.Contracts;
 
 namespace Lykke.Service.ChainalysisProxy.Client
 {
+    /// <summary>
+    /// Chainalysis Proxy Client
+    /// </summary>
     public class ChainalysisProxyClient : IChainalysisProxyClient, IDisposable
     {
         private readonly ILog _log;
         private IChainalysisProxyAPI _service;
 
+        /// <summary>
+        /// Chainalysis Proxy Client
+        /// </summary>
+        /// <param name="serviceUrl"></param>
+        /// <param name="log"></param>
         public ChainalysisProxyClient(string serviceUrl, ILog log)
         {
             _log = log;
             _service = new ChainalysisProxyAPI(new Uri(serviceUrl));
         }
 
+        /// <summary>
+        /// Dispose
+        /// </summary>
         public void Dispose()
         {
             if (_service == null)
@@ -26,6 +37,11 @@ namespace Lykke.Service.ChainalysisProxy.Client
             _service = null;
         }
 
+        /// <summary>
+        /// Register User
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public async Task<UserScoreDetails> RegisterUser(string userId)
         {
             var result = await _service.UserByUserIdRegisterPostWithHttpMessagesAsync(userId);
@@ -38,6 +54,11 @@ namespace Lykke.Service.ChainalysisProxy.Client
 
         }
 
+        /// <summary>
+        /// Get User Score
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public async Task<UserScoreDetails> GetUserScore(string userId)
         {
             var result = await _service.UserByUserIdGetGetWithHttpMessagesAsync(userId);
@@ -49,6 +70,12 @@ namespace Lykke.Service.ChainalysisProxy.Client
             return null;
         }
 
+        /// <summary>
+        /// Add Transaction
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="transaction"></param>
+        /// <returns></returns>
         public async Task<UserScoreDetails> AddTransaction(string userId, NewTransactionModel transaction)
         {
             var result = await _service.UserByUserIdAddtransactionPostWithHttpMessagesAsync(userId, transaction.Map());
@@ -60,6 +87,12 @@ namespace Lykke.Service.ChainalysisProxy.Client
             return null;
         }
 
+        /// <summary>
+        /// Add Wallet
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="wallet"></param>
+        /// <returns></returns>
         public async Task<UserScoreDetails> AddWallet(string userId, NewWalletModel wallet)
         {
             var result = await _service.UserByUserIdAddwalletPostWithHttpMessagesAsync(userId, wallet.Map());
