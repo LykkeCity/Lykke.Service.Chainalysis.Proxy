6e2ca9486a02e1550c99c986f4762622268b1e31
Anton Sivov <anton.sivov@inbox.ru>
2018-03-24 21:47:02 +0300

    Fixed issues after comments of review

--- src/Lykke.Service.ChainalysisProxy.Core/Settings/ServiceSettings/DbSettings.cs
+++ src/Lykke.Service.ChainalysisProxy.Core/Settings/ServiceSettings/DbSettings.cs
@@ -1,8 +1,11 @@
-﻿namespace Lykke.Service.ChainalysisProxy.Core.Settings.ServiceSettings
+﻿using Lykke.SettingsReader.Attributes;
+namespace Lykke.Service.ChainalysisProxy.Core.Settings.ServiceSettings
 {
     public class DbSettings
     {
+        [AzureTableCheck]
         public string LogsConnString { get; set; }
+        [AzureTableCheck]
         public string DataConnString { get; set; }
     }
 }
--- src/Lykke.Service.ChainalysisProxy.Core/Settings/ServiceSettings/Services.cs
+++ src/Lykke.Service.ChainalysisProxy.Core/Settings/ServiceSettings/Services.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Lykke.SettingsReader.Attributes;
 
 namespace Lykke.Service.ChainalysisProxy.Core.Settings.ServiceSettings
 {
     public class Services
     {
-        //[Check]
+        [HttpCheck("/swagger/ui")]
         public string CainalisysUrl { get; set; }
         public string CainalisysKey { get; set; }
     }
