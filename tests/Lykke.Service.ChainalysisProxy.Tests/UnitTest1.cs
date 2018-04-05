4cc6e102cc0859b0e750f0a94237377a64b1a222
Anton Sivov <anton.sivov@inbox.ru>
2018-04-06 00:48:35 +0300

    Fixed issue

--- tests/Lykke.Service.ChainalysisProxy.Tests/UnitTest1.cs
+++ tests/Lykke.Service.ChainalysisProxy.Tests/UnitTest1.cs
@@ -1,12 +1,15 @@
-﻿using Xunit;
+﻿using Lykke.Service.ChainalysisProxy.Client;
+using Xunit;
 
 namespace Lykke.Service.ChainalysisProxy.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestClient()
         {
+            //var service = new ChainalysisProxyClient("http://chainalisys-integration-proxy.lykke-service.svc.cluster.local", null);
+            //var id = service.GetChainalisysId("test").Result;
         }
     }
 
